812d9cabecd2e7323c45d68cddf30723ad6d0440
Dairon Medina Caro <dairon@gooee.com>
2020-01-16 11:36:52 -0500

    Make travis wait 30 mins

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,6 +6,6 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install Erlang/OTP
     kerl build ${OTP_VERSION}
-    kerl install ${OTP_VERSION} ~/kerl/${OTP_VERSION}
+    travis_wait 30 kerl install ${OTP_VERSION} ~/kerl/${OTP_VERSION}
     . ${HOME}/kerl/${OTP_VERSION}/activate
 fi
