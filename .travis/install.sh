fb80a6466e5705c85028ff5dc0910f42485eaac6
Dairon Medina Caro <dairon@gooee.com>
2020-01-16 12:51:51 -0500

    Make kerl update the releases and install rebar3

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,7 +5,10 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     brew install kerl
 
     # Install Erlang/OTP
+    kerl update releases
     travis_wait 25 kerl build ${OTP_VERSION}
     kerl install ${OTP_VERSION} ~/kerl/${OTP_VERSION}
     . ${HOME}/kerl/${OTP_VERSION}/activate
+    wget https://s3.amazonaws.com/rebar3/rebar3
+    chmod +x rebar3
 fi
